7b004ff3fa24a52eb96c84a49ec6f97f8165008c
bragaus <mannoplay@gmail.com>
2020-08-25 19:41:18 -0300

    terminei de arrumar a consulta com parametros

--- src/controllers/modificarArtes.js
+++ src/controllers/modificarArtes.js
@@ -29,28 +29,9 @@ async function listarArtes(req, resultado) {
             ID
         ]
 
-        console.log(valores);
-        // `UPDATE postagem SET baixo = ${baixo}, cima = ${cima}, descricao = ${descricao}, direita = ${direita}, esquerda = ${esquerda}, largura = ${largura}, titulo = ${titulo}, zIndex = ${zIndex} WHERE ID = ${ID}`
-
-        // const {
-        //     direita,
-        //     esquerda,
-        //     cima,
-        //     baixo,
-        //     largura,
-        //     zIndex,
-        //     ID
-        // } = arte
-
         conexao.query(`UPDATE postagem SET baixo = ?, cima = ?, descricao = ?, direita = ?, esquerda = ?, largura = ?, titulo = ?, zIndex = ? WHERE ID = ?`, valores, (req, res) => {
-            console.log(res);
             return resultado.json();
-        });  
-
-        // conexao.query(`UPDATE postagem SET esquerda = ${esquerda}, direita = ${direita}, cima = ${cima}, baixo = ${baixo}, largura = ${largura}, zIndex = ${zIndex} WHERE ID = ${ID}`, (req, res) => {
-        //     return resultado.json();
-        //     console.log(res);
-        // })
+        });
     })
 
 }
